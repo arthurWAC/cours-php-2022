79b112d92386ca41fed60e12d1a4ae844b7042e5
Arthur Weill <a.weill@webandcow.com>
2022-01-06 10:14:46 +0100

    Création du premier fichier index.php
    Construction du Hello World
    Commentaires
    Explications du Hello World

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo 'Hello World';
+// 1 commentaire = 1 ligne qui ne s'execute pas
+/*
+	1 bloc de commentaires
+*/
+
+// echo me permet d'écrire dans mon document HTML, qui sera affiché par le navigateur
+// 'Hello World' est une chaine de caractères
+// En PHP, chaque instruction se termine par ";"
